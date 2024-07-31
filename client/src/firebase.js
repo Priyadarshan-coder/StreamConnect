71814262327f0616d671743f7a32b6cd656b3ee6
Priyadarshan Ghosh Hazra <priyadarshanghoshhazra908@gmail.com>
2024-07-31 22:04:31 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "clone-f8b4d.firebaseapp.com",
+  projectId: "clone-f8b4d",
+  storageBucket: "clone-f8b4d.appspot.com",
+  messagingSenderId: "738509917730",
+  appId: "1:738509917730:web:a16711e03ed7d28b5a047b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
