97e8dd854f4fc05555cf288f46aa16d67349c220
Priyadarshan Ghosh Hazra <priyadarshanghoshhazra908@gmail.com>
2024-09-03 11:40:42 +0530

    Prisma change

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { google, signOut } from '../controllers/auth.controller.js';
+import { google, signOut } from '../controllers/auth.js';
 
 const routes = express.Router();
 
